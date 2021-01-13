507d1a277df7545bb6285f9a02422c3af2566132
alangogoro <cayne9160@gmail.com>
2021-01-13 18:34:27 +0800

    Add ActionButton on MainTab

--- FireTwT/Controller/MainTabController.swift
+++ FireTwT/Controller/MainTabController.swift
@@ -10,13 +10,30 @@ import UIKit
 class MainTabController: UITabBarController {
     
     // MARK: - Properties
+    let actionButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .twitterBlue
+        button.tintColor = .white
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        
+        button.addTarget(self,
+                         action: #selector(actionButtonTapped),
+                         for: .touchUpInside)
+        return button
+    }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViewControllers()
+        configureUI()
         
+    }
+    
+    // MARK: - Selectors
+    @objc func actionButtonTapped() {
+        print("ActionButton Tapped!! 🔰🚧➡️⭐️⚠️❗️")
     }
     
     // MARK: - Helpers
@@ -37,6 +54,17 @@ class MainTabController: UITabBarController {
                                                 rootVC: convs)
         viewControllers = [nav0, nav1, nav2, nav3]
         
+    }
+    
+    func configureUI() {
+        view.addSubview(actionButton)
+        
+        /* ❗️view.safeAreaLayoutGuide
+         * 相較於 view，SafeArea 會適應各種尺寸的哀鳳螢幕，確保 UI 元件可以完整可見 */
+        actionButton.layer.cornerRadius = 56 / 2
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
+                            paddingBottom: 64, paddingRight: 16,
+                            width: 56, height: 56)
     }
     
     func templateNavigationController(imageName: String,
