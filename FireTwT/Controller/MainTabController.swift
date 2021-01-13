7e677f05670e62f3dbe5589fa79ef83318630ecd
alangogoro <cayne9160@gmail.com>
2021-01-13 14:39:35 +0800

    init commit

---
+++ FireTwT/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  FireTwT
+//
+//  Created by usr on 2021/1/13.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+        
+    }
+    
+}
