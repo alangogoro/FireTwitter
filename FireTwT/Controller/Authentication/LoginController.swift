f3901df59c1c98f6ec437783f7e10b754c6a1397
alangogoro <cayne9160@gmail.com>
2021-01-14 12:00:20 +0800

    Added Custom TextFields!

--- FireTwT/Controller/Authentication/LoginController.swift
+++ FireTwT/Controller/Authentication/LoginController.swift
@@ -18,6 +18,30 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = { //鍵入 Image Literal，選取圖片
+        let view = Utilities().customInputContainer(withImage: #imageLiteral(resourceName: "ic_mail_outline_2x"),
+                                                    textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = { //鍵入 Image Literal，選取圖片
+        let view = Utilities().customInputContainer(withImage: #imageLiteral(resourceName: "ic_lock_outline_white_2x"),
+                                                    textField: passwordTextField)
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().customTextField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().customTextField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,5 +65,15 @@ class LoginController: UIViewController {
         logoImageView.centerX(inView: view,
                               topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor,
+                     left: view.leftAnchor, right: view.rightAnchor,
+                     paddingLeft: 16, paddingRight: 16)
     }
 }
